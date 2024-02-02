210553818c24ae6aa7d17024dba4c044467f55be
AndreyNguen <goonikjke@gmail.com>
2024-02-02 10:48:01 +0300

    weatherApp

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import LoadingScreen from "./src/components/LoadingScreen";
+import axios from "axios";
+import WeatherCard from "./src/components/WeatherCard";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "4718e28444257e083c94f01bb61ce957";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main,
+    });
+    console.log("data", { data: { main: { temp }, weather } });
+  };
+
+  getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      console.log("error", error);
+      Alert.alert("Не удалось получить местоположение", "Очень грустно :(");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <LoadingScreen />
+    ) : (
+      <WeatherCard temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
+
+export default function LoadingScreen() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={"dark-content"} />
+      <ActivityIndicator size={"large"} />
+      <Text style={styles.text}>Отправка данных полковнику ФСБ...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FDF6AA",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 20,
+  },
+});
